b526385c6af318eccc2cea39d5fe7e5a27d54c04
Yury Zharikov <zharikovyuri@gmail.com>
2022-08-31 14:59:11 +0300

    before the last sprint

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -3,6 +3,11 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+handler404 = 'core.views.page_not_found'
+handler500 = 'core.views.server_error'
+handler403 = 'core.views.permission_denied'
+
 urlpatterns = [
     path('auth/', include('users.urls', namespace='auth')),
     path('auth/', include('django.contrib.auth.urls')),
@@ -15,5 +20,3 @@ if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
-
-handler404 = 'core.views.page_not_found'
